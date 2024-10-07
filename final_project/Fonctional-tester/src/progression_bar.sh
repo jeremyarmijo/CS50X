7caefb58006341a24e99f05aeede684f5e8cffb3
Jérémy <jeremy.armijo@epitech.eu>
2024-10-07 20:07:23 +0200

    FIX[final project]

---
+++ final_project/Fonctional-tester/src/progression_bar.sh
@@ -0,0 +1,22 @@
+function progression_bar {
+    nb_step=$1
+    var=0
+
+    printf "\033[30m\033[107mChecking your $nb_step tests :\033[0m\n"
+    if [ $nb_step -gt 10 ];then
+        nb_step=10
+    fi
+    printf "["
+    while [ $var != $(($nb_step * 10)) ]; do
+        line_length=$(tput cols)
+        sleep 0.015
+        printf "#"
+        printf "\033[s"
+        printf "\033[$(($line_length - $var - 7))C $(($var / $nb_step))0%%"
+        printf "\033[u"
+        var=$(($var + 1))
+    done
+    printf "]"
+    printf "\033[$(($line_length - $var - 7))C $(($var / $nb_step))0%%\n"
+}
+progression_bar $1
---
+++ final_project/Fonctional-tester/src/return_value_test_with_file.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function return_value_test_with_file {
+    test_nb=$1
+    to_execute=$2
+    file_path=$3
+    value_expected=$4
+    Exit_status=0
+
+    echo -e "\e[35mTest: return_value_test_with_file\e[0m"
+    echo -e "\e[36mExecute [$to_execute $file_path]\e[0m"
+    echo -e "\e[32mExpected $value_expected\e[0m"
+    ./src/execution_prompt.sh "$to_execute"
+    return_value=$(echo $?)
+
+    echo -e "\e[34m######### Personal return_value_test $test_nb ##########\e[0m"
+    if  [ $return_value -eq $value_expected ] && [ -f $file_path ]; then
+        echo -e "\e[34m#\t\e[0m\e\e[32m[PASS] return value = 0\e[0m\t\t\t\e[34m#\e[0m"
+        Exit_status=0
+    else
+        echo -e "\e[34m#\t\e[0m\e\e[31m[FAIL] return value = 84\e[0m\t\t\e[34m#\e[0m"
+        Exit_status=84
+    fi
+    echo -e "\e[34m#################################################\e[0m\n"
+    return $Exit_status
+}
+
+return_value_test_with_file $1 $2 $3 $4 $5
